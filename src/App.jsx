fc36371ca901fae09136fbc5c47afa7b5b60ae32
Eda Işıl İnal <edainalisil@gmail.com>
2024-07-03 09:06:57 +0300

    created chakra ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { useSelector } from "react-redux";
+import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ChakraBaseProvider,
+  extendBaseTheme,
+  theme as chakraTheme,
+} from '@chakra-ui/react'
+
+
+
 
 
 function App() {
   const {value} = useSelector((state) => state.typing)
   return (
     <div className="App">
+      <ChakraProvider>
+       
+      </ChakraProvider>
      <h1>{value}</h1>
     </div>
   );
