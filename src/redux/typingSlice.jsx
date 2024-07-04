f20f0a02b68876cf5a576289386f3d800137528e
Eda Işıl İnal <edainalisil@gmail.com>
2024-07-04 10:29:25 +0300

    fixed gray background issue

--- src/redux/typingSlice.jsx
+++ src/redux/typingSlice.jsx
@@ -19,46 +19,41 @@ export const typingSlice = createSlice({
   },
   reducers: {
     randomWords: (state) => {
-        // Mevcut kelimelerin kopyasını oluştur
-        const shuffledWords = [...state.words];
-      
-        // Kopyalanan diziyi karıştır
-        for (let i = shuffledWords.length - 1; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          [shuffledWords[i], shuffledWords[j]] = [shuffledWords[j], shuffledWords[i]];
-        }
-      
-        // state.words dizisine karıştırılmış kelimeleri atayın
-        state.words = shuffledWords;
-      
-        // Karıştırılmış dizinin ilk kelimesinin arka plan rengini ayarla
-        if (shuffledWords.length > 0) {
-          shuffledWords[0].background = 'gray.300';
-        }
-      },
+      const resetWords = state.words.map(word => ({ ...word, color: 'black', background: "none" }));
+      const shuffledWords = [...resetWords];
+    
+      for (let i = shuffledWords.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [shuffledWords[i], shuffledWords[j]] = [shuffledWords[j], shuffledWords[i]];
+      }
+      state.currentWordIndex = 0; 
+      state.words = shuffledWords;
+      if (shuffledWords.length > 0) {
+        shuffledWords[0].background = 'gray.300';
+      }
+    },
+    
       
     wordControl: (state, action) => {
       const item = action.payload.trim();
-      const language = state.language
-    
-      
       const currentWord = state.words[state.currentWordIndex];
+    
       if (currentWord.turkish === item || currentWord.english === item) {
-        currentWord.color = 'green'; // Doğru kelimenin rengini yeşil yapalım
-        state.correct = state.correct +1;
+        currentWord.color = 'green';
+        state.correct += 1;
       } else {
-        currentWord.color = 'red'; // Yanlış kelimenin rengini kırmızı yapalım
-        state.false = state.false +1;
+        currentWord.color = 'red';
+        state.false += 1;
       }
     
-      currentWord.background = 'none'; // Arka plan rengini kaldır
-      state.currentWordIndex += 1; // Bir sonraki kelimeye geç
-
-      // Eğer daha fazla kelime varsa bir sonraki kelimenin arka plan rengini gri yapalım
+      currentWord.background = 'none';
+      state.currentWordIndex += 1;
+    
       if (state.currentWordIndex < state.words.length) {
         state.words[state.currentWordIndex].background = 'gray.300';
       }
     },
+    
     languageControl : (state,action) => {
 const selectedLanguage = action.payload;
 if(selectedLanguage === "turkish") {
@@ -84,9 +79,12 @@ else{
         state.timer = 60;
         state.isTimerRunning = false;
       },
+      resetCurrentWordIndex: (state) => {
+        state.currentWordIndex = 0;
+      },
     
   }
 });
 
-export const { randomWords, wordControl,languageControl, decrementTimer,resetTimer,startTimer} = typingSlice.actions;
+export const { randomWords, wordControl,languageControl, decrementTimer,resetTimer,startTimer, resetCurrentWordIndex} = typingSlice.actions;
 export default typingSlice.reducer;
