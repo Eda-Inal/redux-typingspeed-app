be3d5e7dfc35e1418fc8076a6b0b8537475215cb
Eda Işıl İnal <edainalisil@gmail.com>
2024-07-04 07:40:14 +0300

    add shuffle btn

--- src/components/Shuffle/index.jsx
+++ src/components/Shuffle/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Button } from '@chakra-ui/react'
 import { FaShuffle } from "react-icons/fa6";
+import { randomWords } from '../../redux/typingSlice';
+import { useDispatch } from 'react-redux';
 function Shuffle() {
+    const dispatch = useDispatch();
   return (
     <div>
       <Button bg="pink.300"  
           mb={10}  _hover={{ bg: 'pink.200' }}  variant='solid'>
-      <FaShuffle />
+      <FaShuffle onClick={() => dispatch(randomWords())} />
   </Button>
   
     </div>
