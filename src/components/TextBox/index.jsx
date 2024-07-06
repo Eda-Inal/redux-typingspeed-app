92e0585995577e593dbf197ae682bc6f4a7d9aa5
Eda Işıl İnal <edainalisil@gmail.com>
2024-07-06 16:44:31 +0300

    change handleKeyDown

--- src/components/TextBox/index.jsx
+++ src/components/TextBox/index.jsx
@@ -1,49 +1,40 @@
+import React, { useState } from 'react';
 import { Input } from '@chakra-ui/react';
-import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import {  wordControl,startTimer } from '../../redux/typingSlice';
+import { wordControl, startTimer } from '../../redux/typingSlice';
 
 function TextBox() {
   const dispatch = useDispatch();
-  const correct = useSelector((state) => state.typing.correct);
   const isTimerRunning = useSelector((state) => state.typing.isTimerRunning);
-  console.log("correct", correct);
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
 
-  
   const handleKeyDown = (event) => {
-    if (event.key === ' ') {
-      if (input.trim() === '') {  // Eğer input boşsa, işlemi durdur
+    if (event.key === ' ' || event.key === 'Spacebar') {
+      event.preventDefault(); // Boşluk tuşunun varsayılan işlevini engellemek için
+      if (input.trim() === '') {
         return;
       }
       if (!isTimerRunning) {
         dispatch(startTimer());
       }
       dispatch(wordControl(input));
-  
-      setInput("");
+      setInput('');
     }
   };
-  
 
   return (
     <div>
-    
-        <Input 
-        
-          bg="white"
-          onKeyDown={handleKeyDown}
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          centerContent
-          maxWidth='400px'
-         
-         
-          _focusVisible={{
-            outline: "none",
-          }}
-        />
-      
+      <Input
+        bg="white"
+        onKeyDown={handleKeyDown}
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        centerContent
+        maxWidth='400px'
+        _focusVisible={{
+          outline: 'none',
+        }}
+      />
     </div>
   );
 }
